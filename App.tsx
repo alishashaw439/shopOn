84f1efda0fac3dd2767338a21649b6100ecb0d65
Alisha Shaw <alishashaw439@gmail.com>
2023-10-05 23:16:16 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { View,Text } from 'react-native';
+
+function App(){
+  return (
+   <View>
+    <Text>Testing</Text>
+   </View>
+  );
+}
+
+
+
+export default App;
